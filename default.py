a4fd6508c04aea6760b9771ab203fee0757f3e04
Galaxy_ <suyogmaharjan38@gmail.com>
2022-01-28 16:53:21 +0545

    backup in case

--- default.py
+++ default.py
@@ -1,4 +1,21 @@
- import keyboard as kb
+import pyautogui as psg
 import time
+from colorama import Fore, Style
 
+#Detect Discord
 time.sleep(2)
+a = psg.locateOnScreen('./Capture.png')
+psg.moveTo(a)
+psg.click()
+
+#Detect mic button and unmute
+time.sleep(2)
+if(psg.locateOnScreen('./unmuted.png')):
+    print(f"\n{Fore.RED}You are already unmuted{Style.RESET_ALL}\n")
+else:
+    b = psg.locateOnScreen('./micbutton.png')
+    psg.moveTo(b)
+    psg.click()
+    print(f"\n{Fore.GREEN}U be unmuted now{Style.RESET_ALL}\n")
+    time.sleep(10)
+    psg.click()
