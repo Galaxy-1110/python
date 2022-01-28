eb237082f9768b96ec098191744e39fac924b639
Mendrop <91016795+Mendrop@users.noreply.github.com>
2022-01-28 21:38:27 +0545

    Add files via upload
    Modified image file names

--- default.py
+++ default.py
@@ -1,17 +1,16 @@
 from random import sample
-from winsound import PlaySound
 import pyautogui as psg
 import time
 from colorama import Fore, Style
 
 #Detect Discord
 time.sleep(2)
-a = psg.locateOnScreen('./Capture.png')
+print('a')
+a = psg.locateOnScreen('./discord.png')
 psg.moveTo(a)
 psg.click()
 
 #Detect mic button and unmute
-time.sleep(2)
 if(psg.locateOnScreen('./unmuted.png')):
     print(f"\n{Fore.RED}You are already unmuted{Style.RESET_ALL}. Continuing the process without unmuting.")
 else:
@@ -19,19 +18,6 @@ else:
     psg.moveTo(b)
     psg.click()
     print(f"\n{Fore.GREEN}U be unmuted now{Style.RESET_ALL}")
-    psg.click()
 
-from playsound import playsound
-from sounddevice import InputStream
-micStream = InputStream(samplerate=16000, blocksize=1024, channels=1,dtype='float32')
-micStream.start()
-playsound('./apple.mp3')
-micStream.stop()
-
-#https://python-sounddevice.readthedocs.io/en/0.4.4/examples.html#play-a-sound-file
-# that would work as wel kekw
-# ye but rip quality kek
-#how about we look up how to play system sound through mic 🤔🤔
-#well there needs to be a py lib for that o3o
-
-that didn't work so nvm
+#Put the keyyou have set as hotkey to play the sound in soundboard betewen ' '. (Soundboard must be running)
+psg.press('`')
